a7f6ca5bdb0207bc1f4ec474083d50ac1fea19dd
Sarit Ritwirune <sarit@elcolie.com>
2018-04-21 19:33:18 +0700

    Make sure with `TestCase`

--- fleets/endpoint_tests.py
+++ fleets/endpoint_tests.py
@@ -21,7 +21,7 @@ def test_add_first_battleship_success(board):
     assert 4 == Fleet.objects.filter(y_axis=1).count()
 
 
-def test_add_first_battleship_hit_edge(board):
+def test_add_first_battleship_hit_edge_top_right(board):
     client = APIClient()
     url = reverse('api:fleet-list')
     data = {
@@ -30,6 +30,54 @@ def test_add_first_battleship_hit_edge(board):
         'vertical': False,
         'x_axis': 10,
         'y_axis': 1,
+    }
+    res = client.post(url, data=data, type='json')
+    msg = {'message': 'Out of battle zone!'}
+    assert status.HTTP_400_BAD_REQUEST == res.status_code
+    assert msg == res.data
+
+
+def test_add_first_battleship_hit_edge_bottom_left(board):
+    client = APIClient()
+    url = reverse('api:fleet-list')
+    data = {
+        'board': board.id,
+        'fleet_type': Fleet.FleetType.battleship,
+        'vertical': True,
+        'x_axis': 1,
+        'y_axis': 10,
+    }
+    res = client.post(url, data=data, type='json')
+    msg = {'message': 'Out of battle zone!'}
+    assert status.HTTP_400_BAD_REQUEST == res.status_code
+    assert msg == res.data
+
+
+def test_add_first_battleship_hit_edge_bottom_right_vertical(board):
+    client = APIClient()
+    url = reverse('api:fleet-list')
+    data = {
+        'board': board.id,
+        'fleet_type': Fleet.FleetType.battleship,
+        'vertical': True,
+        'x_axis': 10,
+        'y_axis': 10,
+    }
+    res = client.post(url, data=data, type='json')
+    msg = {'message': 'Out of battle zone!'}
+    assert status.HTTP_400_BAD_REQUEST == res.status_code
+    assert msg == res.data
+
+
+def test_add_first_battleship_hit_edge_bottom_right_horizontal(board):
+    client = APIClient()
+    url = reverse('api:fleet-list')
+    data = {
+        'board': board.id,
+        'fleet_type': Fleet.FleetType.battleship,
+        'vertical': False,
+        'x_axis': 10,
+        'y_axis': 10,
     }
     res = client.post(url, data=data, type='json')
     msg = {'message': 'Out of battle zone!'}
